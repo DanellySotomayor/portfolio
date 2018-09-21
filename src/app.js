c0c78ef872965d676b9295f399ffee5be71222cb
Danelly <danelly.lucia@gmail.com>
2018-09-21 17:19:56 -0500

    estilos y portafolio

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.navbar-toggler').on('click', function () {
-        $('.animated-icon4').toggleClass('open');
-    });
+  $('.navbar-toggler').on('click', function () {
+    $('.animated-icon4').toggleClass('open');
+  });
 });
