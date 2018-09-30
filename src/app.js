b870efc8a6d487fbeee4591704cc0d0b231492ae
Danelly <danelly.lucia@gmail.com>
2018-09-30 16:21:35 -0500

    seccion skilss

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+
+
 $(document).ready(function () {
   $('.navbar-toggler').on('click', function () {
     $('.animated-icon4').toggleClass('open');
   });
+
+  $(window).scroll(function(){
+    $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 0);
+  });
 });
+
